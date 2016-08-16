bfb115b8b605468edc4b1a5e2aedca8ff0cb4d34
Frederic Rey <Frederic.Rey.Pro@gmail.com>
2016-08-16 11:55:03 +0100

    - Testing web concurrency with Throng

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 'use strict';
-
+const throng = require('throng');
+const os = require('os');
 const app = require('./app');
-const port = app.get('port');
-const server = app.listen(port);
+const env = require('../env');
 
-server.on('listening', () =>
-  console.log(`Feathers application started on ${app.get('host')}:${port}`)
-);
+const port = app.get('port');
+// const server = app.listen(port);
+//
+// server.on('listening', () =>
+//   console.log(`Feathers application started on ${app.get('host')}:${port}`)
+// );
+//
+//
+
+const WORKERS_1 = os.cpus().length;
+const WORKERS_2 = env.WEB_CONCURRENCY;
+
+function start() {
+  app.listen(port, () => {
+    console.log(`WORKER #${process.pid} is listening on localhost:${port}`);
+  });
+}
+
+throng(start);
